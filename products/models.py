a25772d42489137d643f2a293e54695540368ad7
Ryan Lovell <rjl89@live.co.uk>
2021-02-18 15:28:42 +0000

    product sizes added model and creater size selector option to product page and added to bag page

--- products/models.py
+++ products/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     product_number = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    product_sizes = models.BooleanField(default=False, null=True, blank=False)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
