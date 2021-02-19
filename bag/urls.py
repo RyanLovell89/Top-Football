82e9686f1cfb50b3e3f4f53e3028ed712421b570
Ryan Lovell <rjl89@live.co.uk>
2021-02-19 11:50:32 +0000

    created add and remove on bag views and calculate subtotal filter added

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('update/<item_id>/', views.update_to_bag, name='update_to_bag'),
+    path('delete/<item_id>/', views.delete_from_bag, name='delete_from_bag'),
 ]
