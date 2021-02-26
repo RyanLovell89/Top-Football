2ada80871cd067f869a0c7b497cccf6755bf3667
Ryan Lovell <rjl89@live.co.uk>
2021-02-26 19:24:34 +0000

    fixing the checkout successful template

--- profiles/views.py
+++ profiles/views.py
@@ -39,7 +39,7 @@ def order_number_history(request, order_number):
         'A confirmation email was sent on the order date.'
     ))
 
-    template = 'checkout/checkout_success.html'
+    template = 'checkout/checkout_successful.html'
     context = {
         'order': order,
         'from_profile': True,
