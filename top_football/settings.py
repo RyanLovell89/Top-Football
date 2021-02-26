659ab8338e55088e9c1a25f6d39f5b4747ce436c
Ryan Lovell <rjl89@live.co.uk>
2021-02-26 11:23:23 +0000

    aws

--- top_football/settings.py
+++ top_football/settings.py
@@ -176,8 +176,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
 
     AWS_S3_OBJECT_PARAMETERS = {
-        'Ecpires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000'
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
     }
 
     AWS_STORAGE_BUCKET_NAME = 'top-football'
