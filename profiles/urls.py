981b52e5e94c31fdfa31179c538024f0644cc83b
Ryan Lovell <rjl89@live.co.uk>
2021-02-26 19:04:48 +0000

    correcting order history

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name='profiles'),
-    path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('order_number_history/<order_number>', views.order_number_history, name='order_number_history'),
 ]
