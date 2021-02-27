e26e2d2c8e1b56dbbf3c80875268a6bcf92ca710
Ryan Lovell <rjl89@live.co.uk>
2021-02-27 12:48:46 +0000

    add message templates and message to views and settings

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
-
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 # Create your views here.
 
 
@@ -12,6 +13,7 @@ def view_bag(request):
 # Add products to the bag
 def add_to_bag(request, item_id):
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -24,6 +26,7 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['item_by_size'].keys():
                 bag[item_id]['item_by_size'][size] += quantity
+                messages.success(request, f'{product.name} Has Been Added To The Bag')
             else:
                 bag[item_id]['item_by_size'][size] = quantity
         else:
@@ -41,12 +44,14 @@ def add_to_bag(request, item_id):
 # Updates the bag
 def update_to_bag(request, item_id):
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
 
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
+    messages.success(request, f'{product.name} Has Been Added Updated')
 
     if size:
         if quantity > 0:
@@ -69,11 +74,13 @@ def update_to_bag(request, item_id):
 def delete_from_bag(request, item_id):
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
 
         if 'product_size' in request.POST:
             size = request.POST['product_size']
         bag = request.session.get('bag', {})
+        messages.success(request, f'{product.name} Has Been Removed From The Bag')
 
         if size:
             del bag[item_id]['item_by_size'][size]
